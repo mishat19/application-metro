78a38f1944d40c5f99e21ee70fc9511939102a2f
Mathis Brongniart <spidarstudiogames@gmail.com>
2025-04-30 10:10:56 +0200

    Ajout pages #2

---
+++ gestion-modification.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace application_metro
+{
+    partial class gestion_modification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cmbLigne = new ComboBox();
+            lblModif = new Label();
+            lblTitre = new Label();
+            btnRetour = new Button();
+            lblIntitule = new Label();
+            lblCouleur = new Label();
+            txtCouleur = new TextBox();
+            lblNbStations = new Label();
+            dudNumLigne = new DomainUpDown();
+            nudNbStations = new NumericUpDown();
+            lblNoms = new Label();
+            lblCoordonnees = new Label();
+            flpStations = new FlowLayoutPanel();
+            flpCoordonnees = new FlowLayoutPanel();
+            lblHoraires = new Label();
+            rbDirectionA = new RadioButton();
+            rbDirectionB = new RadioButton();
+            flpHoraires = new FlowLayoutPanel();
+            btnEnregistrement = new Button();
+            ((System.ComponentModel.ISupportInitialize)nudNbStations).BeginInit();
+            SuspendLayout();
+            // 
+            // cmbLigne
+            // 
+            cmbLigne.Enabled = false;
+            cmbLigne.FormattingEnabled = true;
+            cmbLigne.Location = new Point(77, 206);
+            cmbLigne.Name = "cmbLigne";
+            cmbLigne.Size = new Size(321, 28);
+            cmbLigne.TabIndex = 41;
+            cmbLigne.Text = "Nom de la ligne";
+            // 
+            // lblModif
+            // 
+            lblModif.AutoSize = true;
+            lblModif.Location = new Point(344, 124);
+            lblModif.Name = "lblModif";
+            lblModif.Size = new Size(94, 20);
+            lblModif.TabIndex = 40;
+            lblModif.Text = "Modification";
+            // 
+            // lblTitre
+            // 
+            lblTitre.AutoSize = true;
+            lblTitre.Font = new Font("Segoe UI", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTitre.Location = new Point(181, 74);
+            lblTitre.Name = "lblTitre";
+            lblTitre.Size = new Size(444, 50);
+            lblTitre.TabIndex = 39;
+            lblTitre.Text = "Panneau de configuration";
+            // 
+            // btnRetour
+            // 
+            btnRetour.Location = new Point(12, 12);
+            btnRetour.Name = "btnRetour";
+            btnRetour.Size = new Size(126, 39);
+            btnRetour.TabIndex = 38;
+            btnRetour.Text = "Retour";
+            btnRetour.UseVisualStyleBackColor = true;
+            // 
+            // lblIntitule
+            // 
+            lblIntitule.AutoSize = true;
+            lblIntitule.Location = new Point(77, 173);
+            lblIntitule.Name = "lblIntitule";
+            lblIntitule.Size = new Size(129, 20);
+            lblIntitule.TabIndex = 42;
+            lblIntitule.Text = "Intitulé de la ligne";
+            // 
+            // lblCouleur
+            // 
+            lblCouleur.AutoSize = true;
+            lblCouleur.Location = new Point(77, 249);
+            lblCouleur.Name = "lblCouleur";
+            lblCouleur.Size = new Size(134, 20);
+            lblCouleur.TabIndex = 45;
+            lblCouleur.Text = "Couleur de la ligne";
+            // 
+            // txtCouleur
+            // 
+            txtCouleur.Location = new Point(77, 282);
+            txtCouleur.Name = "txtCouleur";
+            txtCouleur.Size = new Size(321, 27);
+            txtCouleur.TabIndex = 46;
+            txtCouleur.Text = "Couleur";
+            // 
+            // lblNbStations
+            // 
+            lblNbStations.AutoSize = true;
+            lblNbStations.Location = new Point(77, 337);
+            lblNbStations.Name = "lblNbStations";
+            lblNbStations.Size = new Size(140, 20);
+            lblNbStations.TabIndex = 47;
+            lblNbStations.Text = "Nombre de stations";
+            // 
+            // dudNumLigne
+            // 
+            dudNumLigne.Enabled = false;
+            dudNumLigne.Location = new Point(492, 206);
+            dudNumLigne.Name = "dudNumLigne";
+            dudNumLigne.Size = new Size(150, 27);
+            dudNumLigne.TabIndex = 48;
+            dudNumLigne.Text = "Ligne N°";
+            // 
+            // nudNbStations
+            // 
+            nudNbStations.Location = new Point(232, 335);
+            nudNbStations.Name = "nudNbStations";
+            nudNbStations.Size = new Size(43, 27);
+            nudNbStations.TabIndex = 49;
+            // 
+            // lblNoms
+            // 
+            lblNoms.AutoSize = true;
+            lblNoms.Location = new Point(77, 396);
+            lblNoms.Name = "lblNoms";
+            lblNoms.Size = new Size(310, 20);
+            lblNoms.TabIndex = 50;
+            lblNoms.Text = "Nom des stations (cochez les deux terminus) :";
+            // 
+            // lblCoordonnees
+            // 
+            lblCoordonnees.AutoSize = true;
+            lblCoordonnees.Location = new Point(77, 585);
+            lblCoordonnees.Name = "lblCoordonnees";
+            lblCoordonnees.Size = new Size(216, 20);
+            lblCoordonnees.TabIndex = 51;
+            lblCoordonnees.Text = "Coordonnées GPS des stations :";
+            // 
+            // flpStations
+            // 
+            flpStations.Location = new Point(131, 436);
+            flpStations.Name = "flpStations";
+            flpStations.Size = new Size(511, 125);
+            flpStations.TabIndex = 52;
+            // 
+            // flpCoordonnees
+            // 
+            flpCoordonnees.Location = new Point(131, 623);
+            flpCoordonnees.Name = "flpCoordonnees";
+            flpCoordonnees.Size = new Size(511, 125);
+            flpCoordonnees.TabIndex = 53;
+            // 
+            // lblHoraires
+            // 
+            lblHoraires.AutoSize = true;
+            lblHoraires.Location = new Point(77, 774);
+            lblHoraires.Name = "lblHoraires";
+            lblHoraires.Size = new Size(154, 20);
+            lblHoraires.TabIndex = 54;
+            lblHoraires.Text = "Horaires des stations :";
+            // 
+            // rbDirectionA
+            // 
+            rbDirectionA.AutoSize = true;
+            rbDirectionA.Location = new Point(525, 774);
+            rbDirectionA.Name = "rbDirectionA";
+            rbDirectionA.Size = new Size(183, 24);
+            rbDirectionA.TabIndex = 55;
+            rbDirectionA.TabStop = true;
+            rbDirectionA.Text = "Direction NomStation1";
+            rbDirectionA.UseVisualStyleBackColor = true;
+            // 
+            // rbDirectionB
+            // 
+            rbDirectionB.AutoSize = true;
+            rbDirectionB.Location = new Point(525, 804);
+            rbDirectionB.Name = "rbDirectionB";
+            rbDirectionB.Size = new Size(183, 24);
+            rbDirectionB.TabIndex = 56;
+            rbDirectionB.TabStop = true;
+            rbDirectionB.Text = "Direction NomStation2";
+            rbDirectionB.UseVisualStyleBackColor = true;
+            // 
+            // flpHoraires
+            // 
+            flpHoraires.Location = new Point(131, 844);
+            flpHoraires.Name = "flpHoraires";
+            flpHoraires.Size = new Size(511, 125);
+            flpHoraires.TabIndex = 54;
+            // 
+            // btnEnregistrement
+            // 
+            btnEnregistrement.Location = new Point(516, 1004);
+            btnEnregistrement.Name = "btnEnregistrement";
+            btnEnregistrement.Size = new Size(126, 39);
+            btnEnregistrement.TabIndex = 57;
+            btnEnregistrement.Text = "Enregistrer";
+            btnEnregistrement.UseVisualStyleBackColor = true;
+            // 
+            // gestion_modification
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 1055);
+            Controls.Add(btnEnregistrement);
+            Controls.Add(flpHoraires);
+            Controls.Add(rbDirectionB);
+            Controls.Add(rbDirectionA);
+            Controls.Add(lblHoraires);
+            Controls.Add(flpCoordonnees);
+            Controls.Add(flpStations);
+            Controls.Add(lblCoordonnees);
+            Controls.Add(lblNoms);
+            Controls.Add(nudNbStations);
+            Controls.Add(dudNumLigne);
+            Controls.Add(lblNbStations);
+            Controls.Add(txtCouleur);
+            Controls.Add(lblCouleur);
+            Controls.Add(lblIntitule);
+            Controls.Add(cmbLigne);
+            Controls.Add(lblModif);
+            Controls.Add(lblTitre);
+            Controls.Add(btnRetour);
+            Name = "gestion_modification";
+            Text = "gestion_modification";
+            ((System.ComponentModel.ISupportInitialize)nudNbStations).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox cmbLigne;
+        private Label lblModif;
+        private Label lblTitre;
+        private Button btnRetour;
+        private Label lblIntitule;
+        private Label lblCouleur;
+        private TextBox txtCouleur;
+        private Label lblNbStations;
+        private DomainUpDown dudNumLigne;
+        private NumericUpDown nudNbStations;
+        private Label lblNoms;
+        private Label lblCoordonnees;
+        private FlowLayoutPanel flpStations;
+        private FlowLayoutPanel flpCoordonnees;
+        private Label lblHoraires;
+        private RadioButton rbDirectionA;
+        private RadioButton rbDirectionB;
+        private FlowLayoutPanel flpHoraires;
+        private Button btnEnregistrement;
+    }
+}
---
+++ gestion.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace application_metro
+{
+    partial class gestion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnRetour = new Button();
+            lblTitre = new Label();
+            lblCreation = new Label();
+            lblAjout = new Label();
+            grpAjout = new GroupBox();
+            picPlus = new PictureBox();
+            lblSelection = new Label();
+            cmbHoraire = new ComboBox();
+            cmbStation = new ComboBox();
+            cmbLigne = new ComboBox();
+            lblGestion = new Label();
+            btnSupp = new Button();
+            btnModif = new Button();
+            grpAjout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)picPlus).BeginInit();
+            SuspendLayout();
+            // 
+            // btnRetour
+            // 
+            btnRetour.Location = new Point(12, 12);
+            btnRetour.Name = "btnRetour";
+            btnRetour.Size = new Size(126, 39);
+            btnRetour.TabIndex = 26;
+            btnRetour.Text = "Retour";
+            btnRetour.UseVisualStyleBackColor = true;
+            // 
+            // lblTitre
+            // 
+            lblTitre.AutoSize = true;
+            lblTitre.Font = new Font("Segoe UI", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTitre.Location = new Point(183, 65);
+            lblTitre.Name = "lblTitre";
+            lblTitre.Size = new Size(444, 50);
+            lblTitre.TabIndex = 27;
+            lblTitre.Text = "Panneau de configuration";
+            // 
+            // lblCreation
+            // 
+            lblCreation.AutoSize = true;
+            lblCreation.Location = new Point(90, 150);
+            lblCreation.Name = "lblCreation";
+            lblCreation.Size = new Size(129, 20);
+            lblCreation.TabIndex = 31;
+            lblCreation.Text = "Création de lignes";
+            // 
+            // lblAjout
+            // 
+            lblAjout.AutoSize = true;
+            lblAjout.Location = new Point(190, 40);
+            lblAjout.Name = "lblAjout";
+            lblAjout.Size = new Size(183, 20);
+            lblAjout.TabIndex = 32;
+            lblAjout.Text = "Ajouter une nouvelle ligne";
+            // 
+            // grpAjout
+            // 
+            grpAjout.Controls.Add(picPlus);
+            grpAjout.Controls.Add(lblAjout);
+            grpAjout.Location = new Point(123, 192);
+            grpAjout.Name = "grpAjout";
+            grpAjout.Size = new Size(556, 85);
+            grpAjout.TabIndex = 33;
+            grpAjout.TabStop = false;
+            // 
+            // picPlus
+            // 
+            picPlus.Location = new Point(471, 17);
+            picPlus.Name = "picPlus";
+            picPlus.Size = new Size(59, 62);
+            picPlus.TabIndex = 33;
+            picPlus.TabStop = false;
+            // 
+            // lblSelection
+            // 
+            lblSelection.AutoSize = true;
+            lblSelection.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblSelection.Location = new Point(90, 327);
+            lblSelection.Name = "lblSelection";
+            lblSelection.Size = new Size(239, 25);
+            lblSelection.TabIndex = 34;
+            lblSelection.Text = "Sélectionner vos données :";
+            // 
+            // cmbHoraire
+            // 
+            cmbHoraire.Enabled = false;
+            cmbHoraire.FormattingEnabled = true;
+            cmbHoraire.Location = new Point(136, 452);
+            cmbHoraire.Name = "cmbHoraire";
+            cmbHoraire.Size = new Size(408, 28);
+            cmbHoraire.TabIndex = 35;
+            cmbHoraire.Text = "Choisir un horaire";
+            // 
+            // cmbStation
+            // 
+            cmbStation.Enabled = false;
+            cmbStation.FormattingEnabled = true;
+            cmbStation.Location = new Point(136, 408);
+            cmbStation.Name = "cmbStation";
+            cmbStation.Size = new Size(408, 28);
+            cmbStation.TabIndex = 36;
+            cmbStation.Text = "Choisir une station";
+            // 
+            // cmbLigne
+            // 
+            cmbLigne.FormattingEnabled = true;
+            cmbLigne.Location = new Point(136, 360);
+            cmbLigne.Name = "cmbLigne";
+            cmbLigne.Size = new Size(408, 28);
+            cmbLigne.TabIndex = 37;
+            cmbLigne.Text = "Choisir une ligne";
+            // 
+            // lblGestion
+            // 
+            lblGestion.AutoSize = true;
+            lblGestion.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblGestion.Location = new Point(90, 504);
+            lblGestion.Name = "lblGestion";
+            lblGestion.Size = new Size(87, 25);
+            lblGestion.TabIndex = 38;
+            lblGestion.Text = "Gestion :";
+            // 
+            // btnSupp
+            // 
+            btnSupp.Location = new Point(401, 550);
+            btnSupp.Name = "btnSupp";
+            btnSupp.Size = new Size(126, 121);
+            btnSupp.TabIndex = 39;
+            btnSupp.Text = "Supprimer";
+            btnSupp.UseVisualStyleBackColor = true;
+            // 
+            // btnModif
+            // 
+            btnModif.Location = new Point(205, 550);
+            btnModif.Name = "btnModif";
+            btnModif.Size = new Size(126, 121);
+            btnModif.TabIndex = 40;
+            btnModif.Text = "Modifier";
+            btnModif.UseVisualStyleBackColor = true;
+            // 
+            // gestion
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 700);
+            Controls.Add(btnModif);
+            Controls.Add(btnSupp);
+            Controls.Add(lblGestion);
+            Controls.Add(cmbLigne);
+            Controls.Add(cmbStation);
+            Controls.Add(cmbHoraire);
+            Controls.Add(lblSelection);
+            Controls.Add(grpAjout);
+            Controls.Add(lblCreation);
+            Controls.Add(lblTitre);
+            Controls.Add(btnRetour);
+            Name = "gestion";
+            Text = "gestion";
+            grpAjout.ResumeLayout(false);
+            grpAjout.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)picPlus).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnRetour;
+        private Label lblTitre;
+        private Label lblCreation;
+        private Label lblAjout;
+        private GroupBox grpAjout;
+        private PictureBox picPlus;
+        private Label lblSelection;
+        private ComboBox cmbHoraire;
+        private ComboBox cmbStation;
+        private ComboBox cmbLigne;
+        private Label lblGestion;
+        private Button btnSupp;
+        private Button btnModif;
+    }
+}
